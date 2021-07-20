b7f5c6c760eb8dbc8cb384f5a8e5e3a073f5a8af
Alex <1638309+AlexScotland@users.noreply.github.com>
2021-07-20 00:28:58 -0400

    aded new index page

---
+++ app/bin/spotify_web/spotify_web_api/spotify_api_wrapper.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+import requests
+
+
+class Spot_Wrap():
+    def __init__(self, spotify_key):
+        self.request = requests.Session()
+        self.headers = {"Authorization":f"Bearer: {spotify_key}"
--- app/bin/spotify_web/spotify_web_api/views.py
+++ app/bin/spotify_web/spotify_web_api/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.response import Response
-
 # Create your views here.
 class SpotifyData(viewsets.ViewSet):
     """
@@ -12,4 +11,5 @@ class SpotifyData(viewsets.ViewSet):
         return Response("Please add a slug of your spotify id")
 
     def retrieve(self, request, pk=None):
+# https://accounts.spotify.com/en/authorize?client_id=fe655269e9fc4b529c6453b20dff05e6&response_type=code&redirect_uri=https:%2F%2Flocalhost%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
         return Response(f"{pk}")
